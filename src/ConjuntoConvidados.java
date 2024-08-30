c37d4b19c5ce4bb49d83ae41f86f20c03dd158c3
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-30 18:09:34 -0300

    Completing JavaDoc

--- src/ConjuntoConvidados.java
+++ src/ConjuntoConvidados.java
@@ -2,6 +2,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ConjuntoConvidados {
+    /**
+     * Group of convidados
+     */
     public Set<Convidado> convidadosSet;
 
     /**
